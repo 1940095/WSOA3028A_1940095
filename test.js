c1ab804f6047578dfc8a0686e745674c0d00a94b
1940095 <60913707+1940095@users.noreply.github.com>
2020-03-10 08:48:48 +0200

    2 btns
    btn colorx2

---
+++ test.js
@@ -0,0 +1,10 @@
+let btnColor = document.querySelector('#color');
+
+let btnColor2 = document.querySelector('#color2');
+
+function random(number) {
+    return Math.floor(Math.random() * (number+1));
+  } 
+  
+  btnColor.addEventListener('click', ()=> btnColor.style.backgroundColor='rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')');
+  btnColor2.addEventListener('click', ()=> btnColor2.style.backgroundColor='rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')');
